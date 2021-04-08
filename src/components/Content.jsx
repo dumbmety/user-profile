0ec6409eac7a8c6def07a2590c547a365e5cf178
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-04-08 23:22:28 +0430

    fix: id icon color

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -154,7 +154,20 @@ const Content = () => {
               <FormControl id="companyId">
                 <FormLabel>Company ID</FormLabel>
                 <InputGroup>
-                  <InputLeftAddon>@</InputLeftAddon>
+                  <InputLeftAddon color="gray.500">
+                    <svg
+                      width="1em"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </InputLeftAddon>
                   <Input
                     focusBorderColor="brand.blue"
                     type="text"
